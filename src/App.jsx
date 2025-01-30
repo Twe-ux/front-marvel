b77324438ada6a41a1208e541c61b766aab22d28
Thierry <dev@coworkingcafe.fr>
2025-01-30 10:59:21 +0100

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./assets/css/App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class=" bg-red-500 flex justify-center text-white text-9xl">
+        MARVEL
+      </h1>
+    </>
+  );
+}
+
+export default App;
